3454dc1ec8149b1ea3969412ff208f321f98db27
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-06-22 00:19:32 +0000

    Revert to classic Flutter lifecycle - remove scene delegate to fix white screen

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/freelancer3/flutter"
-export "FLUTTER_APPLICATION_PATH=/var/lib/freelancer/projects/40461372/comeback_app"
+export "FLUTTER_ROOT=/home/freelancer3/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/mnt/freelancer-projects/40461372/comeback_app"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/mnt/freelancer-projects/40461372/comeback_app/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
